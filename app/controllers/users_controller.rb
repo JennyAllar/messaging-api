5b88b3acefb26bea6db030fb0e801ebfb519438a
Jenny Allar <jenny.allar@techstars.com>
2020-07-19 20:31:50 -0600

    on second and third thought, I want the entire record

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.pluck(:name, :email)
+    @users = User.all
     count = User.count
     render json: { total: count, users: @users }, status: 200
   end
